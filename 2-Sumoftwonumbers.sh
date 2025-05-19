833eedd04e7c94a43f88686f6b9ba4c01c168d6d
Agasthya <mshyamala15@gmail.com>
2025-05-19 15:40:37 -0400

    hello

--- 2-Sumoftwonumbers.sh
+++ 2-Sumoftwonumbers.sh
@@ -1,7 +1,7 @@
-#/bin/bash/
+#!/bin/bash/
 NUMBER1=10
 NUMBER2=15
 TIMESTAMP=$(Date)
-echo "Values Entered at :$TIMESTAMP"
+echo "Values Entered at:$TIMESTAMP"
 sum=$((NUMBER1+NUMBER2))
 echo "Sum Of two Numbers: $sum"
---
+++ 8-DivisonOfTwoNumbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please Enter The First Number1::"
+read -s NUMBER1
+echo "Entered Number1 is: $NUMBER1"
+
+echo "Please Enter The First Number2::"
+read -s NUMBER2
+echo "Entered Number1 is: $NUMBER2"
+
+Div=$((NUMBER1/NUMBER2))
+
+echo "Divison of Two Numbers are : $Div"
