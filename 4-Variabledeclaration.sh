fce7d9c7d1d1b8ff2699849b9153779fc4f2486e
Agasthya <mshyamala15@gmail.com>
2025-05-19 14:45:46 -0400

    Variable Global Declarations

---
+++ 4-Variabledeclaration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Harish
+PERSON2=Santhu
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi $NUMBER1, How are you"
+echo "$PERSON1:: I am doing great,$PERSON2"
+echo "$PERSON2:: Please tell me how can i helo you "
+echo "$PERSON1:: Thanks for asking, I will let you know if i face any issues"
