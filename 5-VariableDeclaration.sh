fb4fcc729e994372b9aeda9667b9222865d5cf44
Agasthya <mshyamala15@gmail.com>
2025-05-19 14:50:00 -0400

    variables passed though globally

---
+++ 5-VariableDeclaration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi $PERSON1, How are you"
+echo "$PERSON1:: I am doing great,$PERSON2"
+echo "$PERSON2:: Please tell me how can i helo you "
+echo "$PERSON1:: Thanks for asking, I will let you know if i face any issues"
