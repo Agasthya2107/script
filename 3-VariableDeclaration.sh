d0574fc344f7a40d94832a4fa231477de61e3ffd
Agasthya <mshyamala15@gmail.com>
2025-05-19 14:30:45 -0400

    Basic Variables Declered

---
+++ 3-VariableDeclaration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Harish: Hello Santhu"
+echo "Santhu: Hi Harish, How are you"
+echo "Harish: I am doing great,Santhu"
+echo "Santhu: Please tell me how can i helo you "
+echo "Harish: Thanks for asking, I will let you know if i face any issues"
