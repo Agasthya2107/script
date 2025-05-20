ba5c158d5c7094bf1b3d1f1b701f61cb001c462e
Agasthya <mshyamala15@gmail.com>
2025-05-20 12:26:56 -0400

    Package Instalation

--- 13-InstallScript.sh
+++ 13-InstallScript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
-then 
-    echo "User trying to install application without admin rights"
-    exit 1
-if
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
