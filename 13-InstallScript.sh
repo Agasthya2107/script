5138e3af41f8b58b31d8e30bc0aa40c7c44be3a2
Agasthya <mshyamala15@gmail.com>
2025-05-20 13:00:57 -0400

    Package Instalation

--- 13-InstallScript.sh
+++ 13-InstallScript.sh
@@ -10,7 +10,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo "Allowed to install Mysql"
-    dnf install mysqll -y
+    dnf install mysql -y
     if [ $? -ne 0 ]
     then
         echo "Mysql Instalation Failed"
