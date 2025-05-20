44989e9d413ecaec9a825297557f66fe65269388
Agasthya <mshyamala15@gmail.com>
2025-05-20 12:36:38 -0400

    Package Instalation

--- 13-InstallScript.sh
+++ 13-InstallScript.sh
@@ -6,3 +6,19 @@ then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
 fi
+dnf list install mysql
+if [ $? -ne 0 ]
+then
+    echo "Allowed to install Mysql"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql Instalation Failed"
+        exit 1
+    else
+        echo "Mysql Instalation success"
+    if
+else
+    echo "Mysql Already Installed"
+if
+    
